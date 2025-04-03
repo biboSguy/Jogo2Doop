d847c8a2818576a686c7f3111ad6b430fe89ae57
ENZO CASSOL DE DÉA <enzo_dea@estudante.sesisenai.org.br>
2025-04-03 10:11:19 -0300

    fazer a pontuação

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const canvas = document.getElementById('JogoCanvas')
 const ctx = canvas.getContext('2d')
 
+document.addEventListener('keypress', (e)=> {
+    if(e.code=='Space'){
+        personagem.saltar()
+    }
+})
+
 class Entidade{
     constructor(x, y, largura, altura, cor){
         this.x = x
@@ -14,17 +20,128 @@ class Entidade{
         ctx.fillRect(this.x, this.y, this.largura, this.altura)
     }
 }
+
+
+
+class Personagem extends Entidade{
+    #velocidade_y
+    constructor(x, y, largura, altura, cor){
+        super(x, y, largura, altura, cor)
+        this.#velocidade_y = 0
+        this.pulando = false
+    }
+    saltar(){
+        this.#velocidade_y = 15 // velocidade/quatidade q ele pula
+        this.pulando = true
+        console.log("saltar")
+    }
+    atualizar(){
+        if(this.pulando){
+            this.y -= this.#velocidade_y
+            this.#velocidade_y -= Jogo.gravidade
+            if(this.y >= canvas.height -50){
+                this.#velocidade_y = 0
+                this.y = canvas.height - 50
+                this.pulando = false
+            }
+        }
+    }
+    verificaColisão(){
+        if (
+            obstaculo.x < this.x + this.largura &&
+            obstaculo.largura + obstaculo.x > this.x &&
+            this.y < obstaculo.y + obstaculo.altura &&
+            this.y + this.altura > obstaculo.y
+        ) {
+            obstaculo.velocidade_x = 0
+            this.velocidade_y = 0
+            ctx.fillStyle = 'black'
+            ctx.font = '50px Arial'
+            ctx.fillText('GAME OVER', 50, 100)
+            Jogo.gameOver = true
+            ctx.font = '20px Arial'
+            if(pontuacao_atual > pontuacao_maxima){
+                localStorage.setItem('PM', pontuacao_atual)
+                ctx.fillText(`Novo Record: ${pontuacao_atual}`,50, 150)
+                return
+            }
+            ctx.fillText(`Pontos da jogada: ${pontuacao_atual}`, 50, 150)
+        }
+    }
+}
+
+
+
+class Obstaculo extends Entidade{
+    #velocidade_x
+    constructor(x, y, largura, altura, cor){
+        super(x, y, largura, altura, cor)
+        this.#velocidade_x = 3
+    }
+    atualizar(){
+        this.x -= this.#velocidade_x
+    if(this.x <= 0 - this.largura){
+        this.x = canvas.width
+        this.#velocidade_x += 1
+        let nova_altura = Math.random()*(150-90)+90
+        this.altura = nova_altura
+        this.y = canvas.height - nova_altura
+    }
+    }
+}
+setInterval(() => {
+    if (!gameOver) {
+        pontuacao += 1; 
+    }
+}, 50);
+
+function desenharPontuacao() {
+    ctx.fillStyle = "black";
+    ctx.font = '20px Arial';
+    ctx.fillText('Pontos: ' + pontuacao, 10, 30);
+}
+
+function reiniciarJogo() {
+
+    pontuacao = 0;
+    personagem.x = 100;
+    personagem.y = canvas.height - 50;
+    personagem.velocidade_y = 0;
+    personagem.velocidade_x = 0;
+    Obstaculo.x = 550;
+    Obstaculo.y = canvas.height - 100;
+    objetoSuperior.x = canvas.width;
+    objetoMeio.x = canvas.width;
+    gameOver = false;
+
+    loop();
+}
+
+
+
+
 class Jogo{
+    static gameOver = false
+    static gravidade = 0.5
     constructor(){
         this.loop = this.loop.bind(this)
     }
-    loop(coisa_na_tela){
-        coisa_na_tela.desenhar()
-        requestAnimationFrame(this.loop())
+    loop(){
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        personagem.desenhar()
+        obstaculo.desenhar()
+        personagem.atualizar()
+        obstaculo.atualizar()
+        personagem.verificaColisão()
+        requestAnimationFrame(this.loop)
     }
 }
-const coisa_na_tela = new Entidade(100, 100, 50, 50, 'red')
+
+
+
+const personagem = new Personagem(100, canvas.height - 50, 50, 50, 'blue')
+const obstaculo= new Obstaculo(canvas.width - 50, canvas.height - 100, 50, 100, 'red')
 const jogo = new Jogo()
-jogo.loop(coisa_na_tela)
+jogo.loop()
 
 
